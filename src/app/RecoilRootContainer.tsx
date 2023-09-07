d866ac19e7139d943a5f46fb934fe88cdadae20c
방성훈 (Bang Seonghun) <slimejam01@gmail.com>
2023-09-08 06:42:25 +0900

    Disunite client side method from root Layout.

---
+++ src/app/RecoilRootContainer.tsx
@@ -0,0 +1,18 @@
+'use client';
+import '@/app/components/Firebase';
+import './globals.css';
+import { RecoilRoot } from "recoil";
+import Auth from './components/Auth';
+import Header from '@/app/components/Header';
+import Navigator from '@/app/components/Navigator';
+
+export default function RecoilRootContainer({ children }: { children: any }) {
+    return <>
+        <RecoilRoot>
+            <Auth />
+            <Header />
+            <Navigator />
+            {children}
+        </RecoilRoot>
+    </>
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,9 @@
-'use client';
-// 파이어베이스
-import '@/app/components/Firebase';
-// 리코일
-import { RecoilRoot } from 'recoil';
 // 넥스트
 import type { Metadata } from 'next';
 import { Noto_Sans_KR } from 'next/font/google';
 // 로컬 컴포넌트
-import Auth from './components/Auth';
-import Header from '@/app/components/Header';
-import Navigator from '@/app/components/Navigator';
 import './globals.css';
+import RecoilRootContainer from './RecoilRootContainer';
 
 // 폰트 설정
 const noto_sans_KR = Noto_Sans_KR({
@@ -32,18 +25,8 @@ export default function RootLayout({
 
     return (
         <html lang="ko">
-            <head>
-
-            </head>
             <body className={noto_sans_KR.className}>
-                <RecoilRoot>
-                    <Auth />
-                    <Header />
-                    <Navigator />
-                    {/* 로그인 되어있지 않으면 메인 페이지를 보여주지 않게 하고싶음 */}
-                    {children}
-                </RecoilRoot>
-                {/* <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest/dist/tf.min.js" defer></script> */}
+                <RecoilRootContainer children={children} />
             </body>
         </html>
     )
