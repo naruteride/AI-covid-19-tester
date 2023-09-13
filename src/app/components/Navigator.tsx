99b0562dfe446cc832d22b69ad4f9857de675853
방성훈 (Bang Seonghun) <slimejam01@gmail.com>
2023-09-13 20:17:12 +0900

    Add a method to close an item in the navigator when pressed

--- src/app/components/Navigator.tsx
+++ src/app/components/Navigator.tsx
@@ -1,7 +1,7 @@
 // 넥스트
 import Link from 'next/link';
 // 리코일
-import { atom, useRecoilState, useRecoilValue } from 'recoil';
+import { atom, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 // 로컬
 import { userState } from '@/app/components/Auth';
 import "./Navigator.css";
@@ -21,32 +21,33 @@ export default function Navigator(): React.ReactElement {
                 네비게이터
             </h3>
             <ul>
-                <Link href="/#uploader">
-                    <li>AI 코로나 측정기</li>
-                </Link>
-                <Link href="/#dashboard">
-                    <li>대시보드</li>
-                </Link>
-                {user ? <>
-                    <Link href="/sign/out">
-                        <li>로그아웃</li>
-                    </Link>
-                </> : <>
-                    <Link href="/sign/in">
-                        <li>로그인</li>
-                    </Link>
-                    <Link href="/sign/up">
-                        <li>회원가입</li>
-                    </Link>
-                </>}
-
+                <NavItem href='/#uploader' name='AI 코로나 측정기' />
+                <NavItem href='/#dashboard' name='대시보드' />
+                {
+                    (user)
+                        ? <>
+                            <NavItem href='/sign/out' name='로그아웃' />
+                        </>
+                        : <>
+                            <NavItem href='/sign/in' name='로그인' />
+                            <NavItem href='/sign/up' name='회원가입' />
+                        </>
+                }
             </ul>
         </nav>
         <Background />
     </>
 }
 
+function NavItem({ href, name }: { href: string, name: string }) {
+    const setActivate = useSetRecoilState(navigatorActivate)
 
+    return <>
+        <Link href={href} onClick={() => setActivate(false)}>
+            <li>{name}</li>
+        </Link>
+    </>
+}
 
 function Background() {
     const [activate, setActivate] = useRecoilState(navigatorActivate);
