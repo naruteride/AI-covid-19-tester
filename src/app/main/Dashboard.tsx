c3fa17f62949beb2ab3899f1fff4091729317366
방성훈 (Bang Seonghun) <slimejam01@gmail.com>
2023-09-13 17:43:30 +0900

    Change gitignore to include package.json and lock & Modify LineChart Component

--- src/app/main/Dashboard.tsx
+++ src/app/main/Dashboard.tsx
@@ -18,13 +18,6 @@ export default function Dashboard() {
     const user = useRecoilValue<User | null>(userState);
     const result = useRecoilValue(resultState)
     const [dashboardData, setDashboardData] = useState<{ name: string; }[]>([{ name: "" }]);
-    const [waitForAwait, setWaitForAwait] = useState(false);
-    const [keyCount, setKeyCount] = useState(0)
-    const [windowWidth, setWindowWidth] = useState(0);
-    useEffect(() => {
-        setWindowWidth(window.innerWidth);
-        console.log(windowWidth)
-    }, [])
 
     async function fetchData() {
         if (user == null) {
@@ -32,48 +25,52 @@ export default function Dashboard() {
         }
         const dashboard = await getDashboardData(user);
 
-        if (dashboard !== undefined) {
+        if (dashboard) {
             setDashboardData(dashboard);
         }
-        setWaitForAwait(false);
     }
 
     useEffect(() => {
-        setWaitForAwait(true);
         fetchData();
     }, [user, result]);
 
-    // fetchData가 끝났을 때 전체 대시보드 섹션을 새로 그림
-    useEffect(() => {
-        if (waitForAwait === false) {
-            setKeyCount(keyCount + 1);
-        }
-    }, [waitForAwait])
-
     return <>
-        <section id="dashboard" className={styles.dashboard} key={keyCount}>
+        <section id="dashboard" className={styles.dashboard}>
             <h1>대시보드</h1>
             <p>과거의 진단 기록과 변화 추이를 확인합니다.</p>
             <br /><br />
-            {windowWidth
-                ? <LineChart
-                    width={windowWidth > 1200 ? 800 : windowWidth}
-                    height={windowWidth > 1200 ? 400 : windowWidth / 2}
-                    data={dashboardData}
-                    margin={{ top: 5, right: 60, bottom: 5, left: 0 }}
-                    className={styles.chart}
-                >
-                    <Line type="monotone" key={'건강함'} dataKey={'건강함'} stroke={'#3798d9'} />
-                    <Line type="monotone" key={'코로나-19'} dataKey={'코로나-19'} stroke={'#ef5350'} />
-                    <Line type="monotone" key={'바이러스성 폐렴'} dataKey={'바이러스성 폐렴'} stroke={'#9575cd'} />
-                    <Line type="monotone" key={'박테리아성 폐렴'} dataKey={'박테리아성 폐렴'} stroke={'#66bb6a'} />
-                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                </LineChart>
-                : <Loading />
-            }
+            <LineChartContainer dashboardData={dashboardData} />
+            {/* {
+                (windowWidth)
+                    ? <LineChartContainer dashboardData={dashboardData} windowWidth={windowWidth} />
+                    : <Loading />
+            } */}
         </section>
+    </>
+}
+
+function LineChartContainer({ dashboardData }: { dashboardData: { name: string }[] }) {
+    const [windowWidth, setWindowWidth] = useState(0);
+    useEffect(() => {
+        setWindowWidth(window.innerWidth);
+    }, [])
+
+    return <>
+        <LineChart
+            width={windowWidth > 1200 ? 800 : windowWidth}
+            height={windowWidth > 1200 ? 400 : windowWidth / 2}
+            data={dashboardData}
+            margin={{ top: 5, right: 60, bottom: 5, left: 0 }}
+            className={styles.chart}
+        >
+            <Line type="monotone" key={'건강함'} dataKey={'건강함'} stroke={'#3798d9'} />
+            <Line type="monotone" key={'코로나-19'} dataKey={'코로나-19'} stroke={'#ef5350'} />
+            <Line type="monotone" key={'바이러스성 폐렴'} dataKey={'바이러스성 폐렴'} stroke={'#9575cd'} />
+            <Line type="monotone" key={'박테리아성 폐렴'} dataKey={'박테리아성 폐렴'} stroke={'#66bb6a'} />
+            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+        </LineChart>
     </>
 }
